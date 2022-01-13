931d3a14623ab7dacb4a6ad1ee75a1e676dc6112
『Pro - Lêɠêɳ̃dẞογ - Pro』 <97476963+PROBOY-OP@users.noreply.github.com>
2022-01-13 20:42:51 +0530

    Update format.py

--- userbot/helpers/utils/format.py
+++ userbot/helpers/utils/format.py
@@ -6,7 +6,7 @@ from telethon.tl.tlobject import TLObject
 from telethon.tl.types import MessageEntityPre
 from telethon.utils import add_surrogate
 
-from userbot.ffunctions.utils import utc_to_local
+from userbot.helpers.ffunctions.utils import utc_to_local
 
 from .paste import pastetext
 
