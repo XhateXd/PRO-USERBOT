936d565ca875d620d5e3c3ed1be267982e44a64d
『Pro - Lêɠêɳ̃dẞογ - Pro』 <97476963+PROBOY-OP@users.noreply.github.com>
2022-01-14 06:20:56 +0530

    Update admins.py

--- userbot/plugins/admins.py
+++ userbot/plugins/admins.py
@@ -27,7 +27,7 @@ from userbot.helpers.events import get_user_from_init
 from userbot.helpers.utils import _format
 from userbot.plugins.sql_helper.mute_sql import is_muted, mute, unmute
 from userbot.utils import *
-
+from userbot.helpers.utils import _format 
 from . import *
 
 lg_id = Config.LOGGER_ID
